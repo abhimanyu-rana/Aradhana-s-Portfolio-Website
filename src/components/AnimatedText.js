73f70dcce6281feadf8d63000352969b994da499
abhimanyu-rana <abhimanyurana017@gmail.com>
2024-04-27 07:11:24 +1000

    58.20

--- src/components/AnimatedText.js
+++ src/components/AnimatedText.js
@@ -1,6 +1,11 @@
 import React from 'react';
 
 const AnimatedText = ({ text, className = "" }) => {
+  // Check if text is undefined, null, empty, or not a string
+  if (!text || typeof text !== 'string') {
+    return null; // If any of the above conditions are true, render nothing
+  }
+
   return (
     <div className='w-full mx-auto py-2 flex items-center justify-center text-center overflow-hidden'>
       <h1 className={`inline-block w-full text-dark font-bold capitalize text-8x1 ${className}`}>
