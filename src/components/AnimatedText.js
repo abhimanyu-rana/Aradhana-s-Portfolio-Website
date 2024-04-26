80279dbd439c9ba1dabb1001c4b51068ba2c85b9
abhimanyu-rana <abhimanyurana017@gmail.com>
2024-04-27 07:04:46 +1000

    58.20

--- src/components/AnimatedText.js
+++ src/components/AnimatedText.js
@@ -1,15 +1,14 @@
 import React from 'react';
 
-const AnimatedText = ({text, className = "" }) => {
+const AnimatedText = ({ text, className = "" }) => {
   return (
     <div className='w-full mx-auto py-2 flex items-center justify-center text-center overflow-hidden'>
       <h1 className={`inline-block w-full text-dark font-bold capitalize text-8x1 ${className}`}>
-        {
-            text.split(" ").map((word, index) =>
-          <span key={word +'-'+index}>
+        {text.split(" ").map((word, index) => (
+          <span key={word + '-' + index}>
             {word}&nbsp;
           </span>
-        )}
+        ))}
       </h1>
     </div>
   );
