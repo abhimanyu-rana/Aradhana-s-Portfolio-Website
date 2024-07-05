f284bce2e5887df79a00b1e50690b6956317ff28
abhimanyu-rana <abhimanyurana017@gmail.com>
2024-07-06 01:13:19 +1000

    1.13

--- src/components/AnimatedText.js
+++ src/components/AnimatedText.js
@@ -1,4 +1,32 @@
 import React from 'react';
+import {motion} from "framer-motion"
+
+const quote = {
+  initial:{
+    opacity:1,
+  },
+  animate:{
+    opacity:1,
+    transition:{
+      delay:0.5,
+      staggerChildren: 0.08,
+    }
+  }
+}
+
+const singleWord = {
+  initial:{
+    opacity:0,
+    y:50,
+  },
+  animate:{
+    opacity:1,
+    y:0,
+    transition:{
+      duration:1,
+    }
+  }
+}
 
 const AnimatedText = ({ text, className = "" }) => {
   // Check if text is undefined, null, empty, or not a string
@@ -8,13 +36,23 @@ const AnimatedText = ({ text, className = "" }) => {
 
   return (
     <div className='w-full mx-auto py-2 flex items-center justify-center text-center overflow-hidden'>
-      <h1 className={`inline-block w-full text-dark font-bold capitalize text-8x1 ${className}`}>
+
+      <motion.h1 className={`inline-block w-full text-dark font-bold capitalize text-8x1 ${className}`}
+      variants={quote}
+      initial="initial"
+      animate="animate"
+      >
         {text.split(" ").map((word, index) => (
-          <span key={word + '-' + index}>
+
+          <motion.span key={word +'-'+index} className="inline-block"
+          variants={singleWord}
+          >
+
             {word}&nbsp;
-          </span>
+          </motion.span>
+
         ))}
-      </h1>
+      </motion.h1>
     </div>
   );
 };
